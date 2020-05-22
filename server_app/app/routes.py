f022a31124ef70ccf1c6099f50aa1801cd19a0b2
MartinHarvey <martin.harvey.2017@uni.strath.ac.uk>
2020-05-22 13:55:27 +0100

    Added functionality to upload instructions to server

--- server_app/app/routes.py
+++ server_app/app/routes.py
@@ -1,4 +1,5 @@
 import os
+from flask import request
 from app import app
 
 
@@ -12,9 +13,24 @@ def index():
 # to have different instructions to 
 # different duckys. 
 @app.route('/download/<duckey_name>', methods = ['GET'])
-def filePage(duckey_name):
+def file_Page(duckey_name):
     path = os.getcwd() +"/files/" + duckey_name
-    print(path)
     with open(path, "r") as file:
         data = file.read()
-        return (data, 200)
+        file.close()
+    return(data, 200)
+
+#Upload a new set of instructions for <ducky_name>. 
+@app.route('/upload/instructions/<ducky_name>/', methods = ['POST'])
+def upload_Instruction(ducky_name):
+    file = request.data
+    path = os.getcwd() +"/files/" + ducky_name
+    with open(path, "w") as f:
+        f.write(file)
+        print(f)
+        f.close()
+        return "Success"
+
+
+
+
