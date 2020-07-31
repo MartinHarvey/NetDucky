647d9981ee0ce4883e384eb3c789af75b1aa3cbd
Martin Harvey <martin.harvey.2017@uni.strath.ac.uk>
2020-07-31 18:16:29 +0100

    Started the key injector software. Currently developing on a ESP32 through the Arduino IDE.

---
+++ key_injector/key_injector_main/key_injector_main.ino
@@ -0,0 +1,18 @@
+#include <WiFi.h>
+#include <WiFiMulti.h>
+
+String SSID = "";
+String PASS = "";
+String NET_DUCKY_NAME = "";
+String Server_Host = "";
+String Server_Post = "";
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
