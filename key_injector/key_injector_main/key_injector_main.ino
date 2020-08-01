a4d8542dd56738793ea685d09a19197922c7981b
Martin Harvey <martin.harvey.2017@uni.strath.ac.uk>
2020-08-01 19:22:19 +0100

    Progress on key_injector_main. Now allows connection to wifi AP and can create TCP connection to a remote host

--- key_injector/key_injector_main/key_injector_main.ino
+++ key_injector/key_injector_main/key_injector_main.ino
@@ -1,18 +1,55 @@
 #include <WiFi.h>
 #include <WiFiMulti.h>
+//WiFiMulti object used for handling the Wifi connection
+WiFiMulti WiFiConnection;
 
-String SSID = "";
-String PASS = "";
+//Important parameters for connecting to AP and getting instructions from server
+char AP_SSID[32] = "";
+char PASS[32] = "";
 String NET_DUCKY_NAME = "";
-String Server_Host = "";
-String Server_Post = "";
+const char * SERVER_HOST = "";
+const uint16_t SERVER_PORT = ;
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200); //Serial baud rate
+  Serial.println("START");
+  delay(10);
+  
+  WiFiConnection.addAP(AP_SSID, PASS); //Connect using creds from global vars
 
+  Serial.println("Trying to connect");
+  
+  while(WiFiConnection.run() != WL_CONNECTED){ //Retry until connection made
+    Serial.print("=");
+    delay(500);
+  }
+  
+  Serial.println("\nCONNECTED!!!");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+ WiFiClient client;
 
+ if(!client.connect(SERVER_HOST, SERVER_PORT)){
+    Serial.println("Connection failure. Retry in 5s");
+    delay(5000);
+    return;
+ }
+
+ String url = "/download/" + NET_DUCKY_NAME + "/";
+ client.print(url);
+
+ while(!client.available()){
+    delay(1);
+ }
+ if (client.available() > 0){
+   Serial.println(client.readStringUntil('\r'));
+ }
+ else{
+   Serial.println("TIMEOUT");
+ }
+ Serial.println("Waiting 10secs until restarting");
+ delay(10000);
 }
